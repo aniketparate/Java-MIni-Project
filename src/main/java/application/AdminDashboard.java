ffe44d0537ba1c457755d7fd7c23546b429058de
aniketparate <aniketvparate@gmail.com>
2021-11-30 23:01:18 +0530

    feat: created AdminDashboard

--- src/main/java/application/AdminDashboard.java
+++ src/main/java/application/AdminDashboard.java
@@ -1,4 +1,41 @@
 package application;
 
-public class AdminDashboard {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class AdminDashboard extends MainController{
+
+    @FXML
+    private void userData(ActionEvent mouseEvent) throws Exception{
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AllUserData.fxml")));
+            stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+            stage.setTitle("MY YATRA");
+            //stage.getIcons().add(new Image("icon.png"));
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void trainData(ActionEvent mouseEvent) throws Exception{
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("TrainData.fxml")));
+            stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+            stage.setTitle("MY YATRA");
+            //stage.getIcons().add(new Image("icon.png"));
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
